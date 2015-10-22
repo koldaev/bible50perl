b10de3078018d9ee56c50358db7e44a067b351a9
root <root@static.88-198-33-121.clients.your-server.de>
2015-10-22 16:38:30 +0300

    bible50perl

---
+++ bible50perl/bible.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+
+use utf8;
+use strict;
+use autodie;
+use open qw< IO :utf8 :std>;
+use sigtrap qw< stack-trace normal-signals error-signals >;
+use File::Basename;
+no warnings 'uninitialized';
+
+my $dirscript = dirname(__FILE__);
+
+#if( length $ARGV[0] ) {
+#    our $lang = $ARGV[0];
+#} else {
+#    our $lang = 'ru';
+#}
+#if( length $ARGV[1] ) {
+#    our $book = $ARGV[1];
+#} else {
+#    our $book = 1;
+#}
+
+my $lang = $ARGV[0];
+if (not defined $lang) {
+    $lang = 'ru';
+}
+my $book = $ARGV[1];
+if (not defined $book) {
+    $book = 1;
+}
+
+binmode(STDOUT, ":utf8");
+
+open MORE, '|more' or die "unable to start pager";
+
+#print MORE "hello $_!\n" for 1..1000;
+
+my $filename = $dirscript.'/'.$lang.'/'.$book.'/bible_'.$lang.'_'.$book.'_1.txt';
+open(my $fh, $filename) or die "Could not open file '$filename' $!";
+my $lines = "\n\n\n\n\n\n\n\n\n";
+while (my $row = <$fh>) {
+    $lines .= "$row\n";
+}
+print MORE $lines;
+
+#open(my $fh, $filename) or die "Could not open file '$filename' $!";
+#while (my $row = <$fh>) {
+#    chomp $row;
+#    print MORE "$row\n";
+#}
+
---
+++ bible50perl/biblesavefofiles.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl -w
+require './mysql_connect.pl';
+
+my @biblelangarray = ('ru','en','de','es','fr','it','gr','pt','tr','zh','ara','heb','hin','jap','ko','tam','du','vi','hu','th','da','ro','ur','fa','bn','sv','no','fi','cs','sq','be','uk','bg','hr','kk','ka','hy','lv','et','lt','pl','mk','sr','sk','is','am','af','sw','la','tl');
+
+my $i=0;
+while($i<@biblelangarray) {
+
+    my $langdirectory = $biblelangarray[$i];
+
+    unless (-e $langdirectory or mkdir $langdirectory) {
+        die "Unable to create $langdirectory\n";
+    }
+
+    my $bible = 1;
+    while($bible <= 66) {
+
+        my $directory = $langdirectory."/".$bible;
+
+        unless (-e $directory or mkdir $directory) {
+            die "Unable to create $directory\n";
+        }
+
+        my $biblenametable = $langdirectory.'bible';
+        my $ch = $dbh->prepare(qq{SELECT chapters from $biblenametable where idbible = $bible});
+        $ch->execute();
+        my $biblechapters = $ch->fetchrow_array();
+
+        $ch->finish();
+
+        my $poem = 1;
+        while ($poem <= $biblechapters)
+        {
+
+            my $textname =  $langdirectory.'text';
+            my $sth = $dbh->prepare(qq{SELECT poem, poemtext FROM $textname where bible = $bible and chapter = $poem});
+            $sth->execute();
+
+            my $filenamebible = $directory."/bible_".$langdirectory."_".$bible."_".$poem.".txt";
+            open WRITEFILE, ">$filenamebible" or die "Cannot write to $filenamebible!\n";
+
+            while (my ($poem, $poemtext) = $sth->fetchrow_array())
+            {
+                chomp($langdirectory);
+                if($langdirectory eq "en") {
+                    print WRITEFILE $poem." ".$poemtext."\n";
+                } else {
+                    if (utf8::is_utf8($poemtext)) {
+                        utf8::encode($poemtext);
+                        print WRITEFILE $poem." ".$poemtext."\n";
+                    }
+                }
+            }
+            $sth->finish();
+
+            close(WRITEFILE);
+
+            $poem++;
+
+        }
+
+        $bible++;
+
+    }
+
+    print "bible on ".$langdirectory." created\n";
+    $i++;
+
+}
+
+$dbh->disconnect();
